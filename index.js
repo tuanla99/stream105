fca3dbb332256e7596ab728b173520719b5986f6
Tuan.LA <luonganhtuan1051999@gmail.com>
2020-05-01 17:44:17 +0700

    update server

---
+++ index.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(3000);
+const arrUerInfo =[] ;
+io.on('connection',socket=>{
+    socket.on('NGUOI_DUNG_DANG_KY',user =>{
+        const isExist = arrUerInfo.some(e => e.ten === user.ten) ;
+         socket.peerId = user.peerId;
+        if(isExist) return socket.emit('DANG_KY_THAT_BAI');
+        arrUerInfo.push(user);
+
+        socket.emit('DANH_SACH_ONLINE',arrUerInfo);
+        socket.broadcast.emit('CO_NGUOI_DUNG_MOI',user) ;
+    });
+    
+    socket.on('disconnect',()=>{
+        const index = arrUerInfo.findIndex(user =>user.peerId === socket.peerId) ;
+        arrUerInfo.splice(index,1);
+        io.emit('AI_DO_NGAT_KET_NOI', socket.peerId);
+    });
+});
